579aa3fbc0ca871a8ea220dbcc1f80023300b4d9
homer quan <homer@localhost.localdomain>
2016-05-04 16:36:52 -0400

    debug

---
+++ php/cache/compiled/files/1dd680a5c3989a5ad65ff352ecda2303.yaml.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
+    'filename' => 'themes://milligrav/milligrav.yaml',
+    'modified' => 1462389413,
+    'data' => [
+        'enabled' => true
+    ]
+];
