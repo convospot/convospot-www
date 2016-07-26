a2bf807c1a58088d08418b009430e9151a2374fd
Homer Quan <homer@Homers-MacBook-Air.local>
2016-07-26 15:12:53 -0400

    a regular commit

--- php/cache/compiled/files/c2ca534ad05975fdc4b0b875a2056322.yaml.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
-    'filename' => 'themes://convospot/convospot.yaml',
-    'modified' => 1465699798,
-    'data' => [
-        'enabled' => true
-    ]
-];
